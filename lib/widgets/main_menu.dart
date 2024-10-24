25bdb8264fccee84af0054fddf0600a0c4e6a36f
thang08082003 <nvthang8803@gmail.com>
2024-10-24 18:58:58 +0700

    game

---
+++ lib/widgets/main_menu.dart
@@ -0,0 +1,66 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+import '/widgets/hud.dart';
+import '/game/run.dart';
+
+
+
+class MainMenu extends StatelessWidget {
+
+  static const id = 'MainMenu';
+
+
+  final DinoRun game;
+
+  const MainMenu(this.game, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+        child: Card(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          color: Colors.black.withAlpha(100),
+          child: FittedBox(
+            fit: BoxFit.scaleDown,
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 20, horizontal: 100),
+              child: Wrap(
+                direction: Axis.vertical,
+                crossAxisAlignment: WrapCrossAlignment.center,
+                spacing: 10,
+                children: [
+                  const Text(
+                    'EndlessRunner',
+                    style: TextStyle(
+                      fontSize: 50,
+                      color: Colors.white,
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      game.startGamePlay();
+                      game.overlays.remove(MainMenu.id);
+                      game.overlays.add(Hud.id);
+                    },
+                    child: const Text(
+                      'Play',
+                      style: TextStyle(
+                        fontSize: 30,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
